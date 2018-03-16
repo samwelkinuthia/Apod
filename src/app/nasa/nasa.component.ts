75e0589e70177bcc2e439e86c43b88e3f7b7cf8f
samwel <samwuelkinuthia@gmail.com>
2018-03-16 18:59:21 +0300

    generate component nasa for apod

---
+++ src/app/nasa/nasa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nasa',
+  templateUrl: './nasa.component.html',
+  styleUrls: ['./nasa.component.css']
+})
+export class NasaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
