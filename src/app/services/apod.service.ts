0e96fd8130d2e1586aa027d16919f9097cc9543b
samwel <samwuelkinuthia@gmail.com>
2018-03-16 19:24:24 +0300

    add nasa apod url and log response

--- src/app/services/apod.service.ts
+++ src/app/services/apod.service.ts
@@ -5,5 +5,8 @@ import { HttpClient } from '@angular/common/http';
 export class ApodService {
 
   constructor(private http: HttpClient) {
+    this.http.get('https://api.nasa.gov/planetary/apod?api_key=N18TwfvKYuPWR5mjCxFcnLwYBzK2cWpAarckfTG8').subscribe((data) => {
+      console.log(data);
+    });
   }
 }
